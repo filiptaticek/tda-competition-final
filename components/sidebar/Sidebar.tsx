b633dce49fac2ab116ce12bba2751c88d4ed010d
Filip Tatíček <filtaticek@seznam.cz>
2023-01-04 14:19:12 +0100

    progress in users page

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -13,7 +13,7 @@ export const Sidebar = ()=>{
     <div
       className={`${
         isOpen ? "w-[350px] p-20" : "w-0"
-      } fixed h-full top-0 left-0 z-10 bg-main_color border border-black duration-300`}
+      } h-full fixed top-0 left-0 z-10 bg-main_color border border-black duration-300`}
     >
       <div className={` ${isOpen?"block":"hidden"} text-white text-2xl`}>
         <img className="fixed top-10 cursor-pointer left-[300px] w-[20px]" src="zavrit_stranku.png" onClick={()=>dispatch(toggleSidebarVisbility())}/>
