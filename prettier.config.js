490bd9f9fe552dff38d7b11c6cdb9dda1a05b36e
Filip Tatíček <filtaticek@seznam.cz>
2023-05-09 23:07:12 +0200

    fixing the app so that there is no confirm button and some style adjustment

--- prettier.config.js
+++ prettier.config.js
@@ -1,22 +1,11 @@
-/** @type {import('prettier').Config} */
-/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
-
-/*module.exports = {
-  semi: false, //středníky na konci řádků
-  trailingComma: "none", //čárky na konci řádků
-  singleQuote: false, //uvozovky
-  printWidth: 150, //maximální šířka řádku, pak to hodí na nový řádek
-  tabWidth: 2, //počet mezer pro jeden tabulátor
-  arrowParens: "always", //vždy hodí závorky okolo šipkové funkce?
-  endOfLine: "lf", //
-  importOrder: ["^react/(.*)$|^react$", "^next/(.*)$|^next$", "<THIRD_PARTY_MODULES>", "^@/(.*)$", "^[./]"],
-  importOrderBuiltinModulesToTop: true,
-  importOrderCaseInsensitive: true,
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderMergeDuplicateImports: true,
-  importOrderCombineTypeAndValueImports: true,
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "none",
+  doubleQuote: true,
+  jsxBracketSameLine: true,
+  semi: false,
+  importOrder: ["^[./]"],
   importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  indentSwitchCase: true, //přidáno pro změnu odsazení klíčového slova 'case',
-  jsxBracketSameLine: true
-}*/
+  importOrderSortSpecifiers: true
+}
